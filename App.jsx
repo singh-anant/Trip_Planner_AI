94c87a7e84a1a0be7b92d7bbaeb04663a926bced
anantsingh <anant.singh@mismosystems.com>
2024-09-02 18:29:24 +0530

    create a react native app using cli.

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default App;
